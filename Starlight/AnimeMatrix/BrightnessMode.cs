539180cc5aa69e931379eea268f4bcefe83f714e
vddCore <vddcore@outlook.com>
2023-01-12 12:27:26 +0100

    Initial commit.

---
+++ Starlight/AnimeMatrix/BrightnessMode.cs
@@ -0,0 +1,10 @@
+﻿namespace Starlight.AnimeMatrix
+{
+    public enum BrightnessMode : byte
+    {
+        Off = 0,
+        Dim = 1,
+        Medium = 2,
+        Full = 3
+    }
+}
---
+++ Starlight/Communication/Packet.cs
@@ -0,0 +1,96 @@
+﻿using System.ComponentModel;
+using HidSharp;
+
+namespace Starlight.Communication
+{
+    public abstract class Packet
+    {
+        private int _currentDataIndex = 1;
+        private byte[] _data;
+
+        internal Packet(byte reportId, int packetLength, params byte[] data)
+        {
+            if (packetLength < 1)
+            {
+                throw new ArgumentOutOfRangeException(
+                    nameof(packetLength),
+                    "Packet length must be at least 1."
+                );
+            }
+            
+            _data = new byte[packetLength];
+            _data[0] = reportId;
+
+            if (data.Length > 0)
+            {
+                if (_currentDataIndex >= _data.Length)
+                {
+                    throw new ArgumentOutOfRangeException(
+                        nameof(data),
+                        "Your packet length does not allow for initial data to be appended."
+                    );
+                }
+                
+                AppendData(data);
+            }
+        }
+
+        public Packet AppendData(params byte[] data)
+            => AppendData(out _, data);
+
+        public Packet AppendData(out int bytesWritten, params byte[] data)
+        {
+            bytesWritten = 0;
+
+            for (var i = 0;
+                 i < data.Length && _currentDataIndex < _data.Length - 1;
+                 i++, bytesWritten++, _currentDataIndex++)
+            {
+                if (_currentDataIndex > _data.Length - 1)
+                    break;
+
+                _data[_currentDataIndex] = data[i];
+            }
+
+            return this;
+        }
+
+        public void Set(HidStream stream)
+        {
+            WrapException(() =>
+            {
+                stream.SetFeature(_data);
+                stream.Flush();
+            });
+        }
+
+        public byte[] Get(HidStream stream)
+        {
+            WrapException(() =>
+            {
+                stream.GetFeature(_data);
+                stream.Flush();
+            });
+
+            return _data;
+        }
+
+        private void WrapException(Action action)
+        {
+            try
+            {
+                action();
+            }
+            catch (IOException e)
+            {
+                if (e.InnerException is Win32Exception w32e)
+                {
+                    if (w32e.NativeErrorCode != 0)
+                    {
+                        throw;
+                    }
+                }
+            }
+        }
+    }
+}
