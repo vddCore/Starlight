8539a6b3f41d62dca677e7054379e82760ee775c
vddCore <vddcore@outlook.com>
2023-09-04 17:59:03 +0200

    Add ASUS-driver-compatible keyboard model type indication

--- Starlight/Asus/AuraKeyboard/AuraKeyboardInformation.cs
+++ Starlight/Asus/AuraKeyboard/AuraKeyboardInformation.cs
@@ -2,13 +2,42 @@
 
 public class AuraKeyboardInformation
 {
+    public enum KeyboardFamily : byte
+    {
+        Strix = 0x0F,
+        TUF = 0x15,
+        Zephyrus = 0x16,
+        NR2301 = 0x17,
+        Flow = 0x18,
+        Desktop = 0x1F,
+        Generic = 0xFF
+    }
+    
     public byte BacklightType { get; }
     public byte ProductionYearHex { get; }
     public byte LayoutType { get; }
     public byte FeatureBitmap1 { get; }
     public byte FeatureBitmap2 { get; }
-    public byte? ModelType { get; }
+    public byte? ModelTypeByte { get; }
 
+    public KeyboardFamily Family
+    {
+        get
+        {
+            return ModelTypeByte switch
+            {
+                null => throw new InvalidOperationException("This field is not supported by the current device."),
+                0x01 => KeyboardFamily.Strix,
+                0x02 => KeyboardFamily.Flow,
+                0x04 => KeyboardFamily.Zephyrus,
+                0x08 => KeyboardFamily.TUF,
+                0x10 => KeyboardFamily.NR2301,
+                0x20 => KeyboardFamily.Desktop,
+                _ => KeyboardFamily.Generic
+            };
+        }
+    }
+    
     public bool SupportsLogoLED => (FeatureBitmap1 & 0x01) != 0;
     public bool SupportsLightBar => (FeatureBitmap1 & 0x02) != 0;
     public bool SupportsVCut => (FeatureBitmap1 & 0x10) != 0;
@@ -27,6 +56,6 @@ public class AuraKeyboardInformation
         LayoutType = layoutType;
         FeatureBitmap1 = featureBitmap1;
         FeatureBitmap2 = featureBitmap2;
-        ModelType = modelType;
+        ModelTypeByte = modelType;
     }
 }
